a502fb1b64000caeddc74dfae61f17bd610a95f4
jandie <eidnaj@live.nl>
2017-04-20 15:28:00 +0200

    Added documentation

--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from random import randint
 
 AMOUNT_OF_DAYS = 11
-RANGE_PERCENTAGE = 10
+DEVIATION_PERCENTAGE = 10
 NAMES_FILE = "./CSV_Database_of_First_Names.csv"
 SECONDS_IN_MINUTE = 60
 MINUTES_IN_HOUR = 60
@@ -22,10 +22,19 @@ HOUR_SHEET = [
 
 
 def rand_name(names):
+    """
+    Gets a random name from the list of names
+    :param names:  The list of names.
+    :return: A random name from the list.
+    """
     return names[randint(0, len(names) - 1)]
 
 
 def load_names():
+    """
+    Loads names from a file.
+    :return: A list containing the names.
+    """
     temp_names = []
 
     with open(NAMES_FILE) as f:
@@ -37,13 +46,23 @@ def load_names():
 
 
 def random_between_bounds(number):
-    min_bound = round(number - number / (100 / RANGE_PERCENTAGE))
-    max_bound = round(number + number / (100 / RANGE_PERCENTAGE))
+    """
+    Generates a random number with a random deviation.
+    :param number: The number to deviate around.
+    :return: A random number.
+    """
+    min_bound = round(number - number / (100 / DEVIATION_PERCENTAGE))
+    max_bound = round(number + number / (100 / DEVIATION_PERCENTAGE))
 
     return randint(min_bound, max_bound)
 
 
 def generate_datetime(hour):
+    """
+    Generates a datetime with random minutes and seconds.
+    :param hour: The hour of the datetime.
+    :return: A datetime with random minutes and seconds.
+    """
     minute = randint(0, MINUTES_IN_HOUR - 1)
     second = randint(0, SECONDS_IN_MINUTE - 1)
 
@@ -53,6 +72,11 @@ def generate_datetime(hour):
 
 
 def generate_day_cycle(names):
+    """
+    Generates one day cycle of logs.
+    :param names: A list of names to be randomly added to the logs.
+    :return: A log of a single day cycle in a list.
+    """
     day_log = []
 
     for i in range(0, len(HOUR_SHEET)):
@@ -68,6 +92,11 @@ def generate_day_cycle(names):
 
 
 def generate_days(nr_of_days):
+    """
+    Generates multiple day cycles.
+    :param nr_of_days: Number of day cycles to generate.
+    :return: A log containing multiple day cycles.
+    """
     log = []
     names = load_names()
 
@@ -80,7 +109,7 @@ def generate_days(nr_of_days):
     return log
 
 
-log = generate_days(1)
+log = generate_days(365)
 
 for x in log:
     print(str(x[0].time()) + ", " + x[1] + ", " + str(x[2]))
